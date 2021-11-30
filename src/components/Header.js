068dd1eb68ad75723acb93f794d42b86ee29d340
OthmaneID <othmanid5@users.noreply.github.com>
2021-11-30 20:11:51 +0100

    first commit

--- src/components/Header.js
+++ src/components/Header.js
@@ -12,7 +12,7 @@ const Header = () => {
 
         {/* ------ */}
       </div>
-
+    
       <div className="header_nav">
         <div className="header_option">
           <span className="header_optionLineOne">Hello user</span>
