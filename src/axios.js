c9aa81f860295941b1ff6762183387af8bb6139e
OthmaneID <othmanid5@users.noreply.github.com>
2021-12-18 15:18:33 +0100

    commit

--- src/axios.js
+++ src/axios.js
@@ -2,7 +2,8 @@ import axios from 'axios';
 
 
 const instance = axios.create({
-    baseURL: "..." //API (cloud function) URL
+    //API (cloud function) URL
+    baseURL: "http://localhost:5001/clone-2a083/us-central1/api"
 })
 
 
